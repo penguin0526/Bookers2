f465cbca931e0cf337cd093512202e1604266806
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-06-07 18:03:53 +0000

    [Update]book,show ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.post_images.page(params[:page])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
