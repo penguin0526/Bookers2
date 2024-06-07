ff2f7d1f2b18f768f3b26993221531c6c18bc10b
KokiOkuda <165885793+penguin0526@users.noreply.github.com>
2024-06-07 20:18:39 +0000

    [Update]user,index ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
   def edit
   end
 
   def show
     @user = User.find(params[:id])
-    @books = @user.post_images.page(params[:page])
+    @books = @user.books.page(params[:page])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get "home/about" => "home#about", as: "about"
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
